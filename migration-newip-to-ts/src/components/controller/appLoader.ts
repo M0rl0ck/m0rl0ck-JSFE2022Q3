4b0a0fd9f534aa48d95c70e0cdc86703e7a50f70
m0rl0ck <seriy17@yandex.ru>
2022-07-04 08:53:40 +0300

    fix: fix url

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'bb4d30e6207a45d7b438aba84f581e29', // получите свой ключ https://newsapi.org/
         });
     }
