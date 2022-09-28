8ab260fdd1972dd96e030266dc737522ba05d002
M0rl0ck <seriy17@yandex.ru>
2022-09-28 23:06:32 +0300

    feat: add input number fill

--- online-zoo/src/pages/donate/index.ts
+++ online-zoo/src/pages/donate/index.ts
@@ -1,22 +1,39 @@
-const runge: HTMLInputElement = document.querySelector('.payRange');
+const range: HTMLInputElement = document.querySelector('.payRange');
 const sum = document.querySelector('.rangeSum');
-const options = document.querySelectorAll('.option');
+const options: NodeListOf<HTMLOptionElement> = sum.querySelectorAll('.option');
+const inputNumber: HTMLInputElement = document.querySelector('.input__num'); 
 
 
-function setRung(e: Event) {
+function setRang(e: Event) {
   const el = e.target as HTMLOptionElement;
   
   if (el.classList.contains('option')) {
-    runge.value = el.value;
+    range.value = el.value;
+    inputNumber.value = el.label.slice(1);
     options.forEach((item) => {
       item.classList.remove('option_active');
     });
     el.classList.add('option_active');
-
+  }
+}
+  function setLableRang(e: Event) {
+    const el = e.target as HTMLElement;
+    if (el.classList.contains('payRange')) {
+    const value = range.value;
+    options.forEach((item) => {
+      if (item.value === value) {
+        item.classList.add('option_active');
+        inputNumber.value = item.label.slice(1);
+      } else {
+        item.classList.remove('option_active');
+      }
+    });
+  }
   }
   
-}
 
-sum.addEventListener('click' ,setRung);
+
+sum.addEventListener('click', setRang);
+range.addEventListener('click', setLableRang )
 
 console.log(sum);
