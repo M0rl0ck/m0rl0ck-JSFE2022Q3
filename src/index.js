5ce1a9f8f7cdd789f238027e99808a5bdd081a2a
m0rl0ck <seriy17@yandex.ru>
2022-10-23 11:32:19 +0300

    refactor: delete old files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { buttonStart } from "./scrpt/components/header";
-import gameField from './scrpt/base/field';
-
-  function start() {
-  gameField.start();
-}
-buttonStart.addEventListener('click', start);
